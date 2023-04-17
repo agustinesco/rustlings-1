86260787a016e39e94df59fac4a30f4b0ea33bd8
agustinesco <agustinesco@outlook.es>
2023-04-16 21:07:14 -0300

    Finish ownership module

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,10 @@
 // move_semantics1.rs
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // ownership from vec0 is given to fill_vec
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,11 +14,11 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = vec; // we assing ownership from vec0 to vec
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec // we return an ownership
 }
