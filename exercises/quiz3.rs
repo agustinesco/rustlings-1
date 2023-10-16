9aa6a6a45f42352e03a47dee4811ee9968a5fc0d
agustinesco <agustin.escobar@lambdaclass.com>
2023-10-15 23:56:18 -0300

    Quiz 3 done

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,28 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: std::fmt::Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.get_grade())
+    }
+}
+
+pub trait GetGrade {
+    fn get_grade(self) -> String;
+}
+
+impl<T: std::fmt::Display> GetGrade for &ReportCard<T> {
+    fn get_grade(self) -> String {
+        format!("{}", self.grade)
     }
 }
 
@@ -50,7 +60,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
